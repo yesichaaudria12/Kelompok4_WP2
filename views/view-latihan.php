4c981e25fae1fa3b6bfb022e97dd17d9ffcda527
Yesicha Audria <yesichaaudria122@gmail.com>
2021-10-03 16:24:51 +0700

    Menambahkan Contoh 3 oleh Muhamad Fathur Rasidi

---
+++ views/view-latihan.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. yuk kita belajar web programming..!!!<br>
+    Niali 1 = <?= $nilai1; ?>
+    Niali 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
